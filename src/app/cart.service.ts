0e91b53055900a9217a3bbdbae9f97e3b4363d33
NitinNair89 <nitin.dreamern89@gmail.com>
2019-12-14 12:05:06 +0530

    Creating and using the cart service

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,8 +1,27 @@
 import { Injectable } from '@angular/core';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class CartService {
 
-  constructor() { }
+  // Array (product cart) to hold the products
+  items = [];
+
+  // Method to add produt to items array (cart)
+  addToCart(product) {
+    this.items.push(product);
+  }
+
+  // Method to get products from the cart
+  getItems() {
+    return this.items;
+  }
+
+  // Method to clear the cart
+  clearItems() {
+    this.items = [];
+    this.getItems();
+  }
 
 }
